d0a163dadf6101e5fe30a0ce4c209ba836b1fe50
Aurimas Valionis <aurimas.valionis@gmail.com>
2026-08-31 16:11:10 +0300

    Split flake into 'utm' and 'macpro' host configurations

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
-    
+
     # Add Home Manager and link it to your Nixpkgs branch
     home-manager = {
       url = "github:nix-community/home-manager/release-26.05";
@@ -13,11 +13,32 @@
 
   outputs = { self, nixpkgs, home-manager, ... }@inputs: {
     nixosConfigurations = {
+
+      # 1. Virtual Machine (Testing Environment)
+      utm = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          ./hardware-configuration-utm.nix # Uses the UTM hardware map
+
+          # Inject Home Manager into the NixOS build
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            # Point to your dedicated user configuration file
+            home-manager.users.avalue = import ./home.nix;
+          }
+        ];
+      };
+
+      # 2. Physical Mac Pro 5,1 (Bare-Metal Environment)
       macpro = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
-          
+          ./hardware-configuration-macpro.nix # You will generate this on the live USB
+
           # Inject Home Manager into the NixOS build
           home-manager.nixosModules.home-manager
           {
